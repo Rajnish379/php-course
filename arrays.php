c2ec2ddb2c99b022e675b0a391bbf9bce247ca5a
Rajnish379 <rajinish77@gmail.com>
2022-12-27 18:19:40 +0530

    arrays

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$friends = array("Kevin",1,false,"Oscar","Jim");
+$friends[5] = "Mark";
+$friends[10] = 4;
+// echo $friends[5]
+echo count($friends)
+
+?>
+    
+</body>
+</html>
